5bc4784ae73cc8f466ff72b94f7a7c882233fbb5
Trần Xuân Việt <viet.tx@3si.vn>
2020-10-07 11:05:06 +0700

    Add project files.

---
+++ NetCoreJqueryDatatable/Data/DatatableContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreJqueryDatatable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreJqueryDatatable.Data
+{
+    public class DatatableContext : DbContext
+    {
+        public DatatableContext(DbContextOptions<DatatableContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
